88e28fc38e5011b6839e5b480e8f2c50119b0c3b
RedScarOfficial <scott@redscar.net>
2025-07-04 21:39:27 -0400

    Update footer position to bottom of page

--- src/App.tsx
+++ src/App.tsx
@@ -37,12 +37,12 @@ function AppContent() {
   };
 
   return (
-    <div className={`flex min-h-screen bg-slate-50 dark:bg-slate-900 ${getPerformanceClass()}`}>
+    <div className={`flex flex-col min-h-screen bg-slate-50 dark:bg-slate-900 ${getPerformanceClass()}`}>
       <Sidebar activeTab={activeTab} onTabChange={setActiveTab} />
-      <main className="flex-1">
+      <main className="flex-1 flex flex-col">
         {renderContent()}
+        <Footer />
       </main>
-      <Footer />
       
       {/* Battery Saver Indicator */}
       {!shouldSimplifyUI() && (
